76e8e4fe8483401842dcd08eabcfcebb410e050b
nothing717 <2625782226@qq.com>
2023-05-06 10:41:52 +0800

    提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -10,19 +10,19 @@ except ImportError as e:
 
 def ridge(data):
     X, y = read_data()
-    weight = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
-    return weight @ data
+    w = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
+    return w @ data
     
 def lasso(data):
-    learning_rate = 1e-10
-    max_iter = 10000
-    alpha = 0.1
     X, y = read_data()
-    weight = np.zeros(X.shape[1])
+    learning_rate = 1e-12
+    max_iter = 100000
+    alpha = 0.1
+    w = np.zeros(X.shape[1])
     for i in range(max_iter):
-        gradient = np.dot(X.T, (np.dot(X, weight) - y)) + alpha * np.sign(weight)
-        weight =weight - learning_rate * gradient
-    return weight @ data
+        grad = np.dot(X.T, (np.dot(X, w) - y)) + alpha * np.sign(w)
+        w =w - learning_rate * grad
+    return w @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
