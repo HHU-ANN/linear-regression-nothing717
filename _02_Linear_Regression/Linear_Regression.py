f5a42fb7b0ee582e03b007e0c3c6f4a95eca89e8
nothing717 <2625782226@qq.com>
2023-05-06 12:34:33 +0800

    提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -22,22 +22,17 @@ def lasso(data):
     max_iter = 10000
     alpha = 0.1
     n_samples, n_features = X.shape
-    #初始化参数
     w = np.zeros(n_features)
     b = 0
     # 梯度下降迭代
     for i in range(max_iter):
         y_pred = np.dot(X, w) + b
-        # 计算L2正则化项对应的梯度
         dw_reg = alpha * np.sign(w)
-        # 计算残差与自变量之间的内积
         residuals = y - y_pred
         dw = (-2/n_samples) * np.dot(X.T, residuals) + dw_reg
         db = (-2/n_samples) * np.sum(residuals)
-        # 更新参数
         w -= learning_rate * dw
         b -= learning_rate * db
-    # 返回预测值
     return np.dot(data, w) + b
 
 def read_data(path='./data/exp02/'):
